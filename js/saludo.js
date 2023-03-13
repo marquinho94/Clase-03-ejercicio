a705fde3448cad6caea14d8a342d119f99b87342
marquinho94 <marcoalanmiranda@gmail.com>
2023-03-12 23:24:00 -0300

    first commit

---
+++ js/saludo.js
@@ -0,0 +1,13 @@
+// ( function() {
+//     const saludoBoton = document.querySelector('.saludo');
+
+//     saludoBoton.addEventListener('click', ()=>{
+//         alert('probando el botón');
+//     });
+    
+// }) ();
+
+
+var name = prompt("Cual es tu nombre?");
+
+alert("Hola " + name);
